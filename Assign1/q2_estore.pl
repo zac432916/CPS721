bc8e6932e3215c0d95637e79180b59ad3ed1fb0b
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2023-09-15 15:39:07 -0400

    Add files via upload

---
+++ Assign1/q2_estore.pl
@@ -0,0 +1,54 @@
+% Enter the names of your group members below.
+% If you only have 2 group members, leave the last space blank
+%
+%%%%%
+%%%%% NAME: 
+%%%%% NAME:
+%%%%% NAME:
+%
+% Add the required rules in the corresponding sections. 
+% If you put the rules in the wrong sections, you will lose marks.
+%
+% You may add additional comments as you choose but DO NOT MODIFY the comment lines below
+%
+
+%%%%% SECTION: cost
+% Put your atomic statements for the cost in this section
+
+
+%%%%% SECTION: numPurchased
+% Put your atomic statements for the numPurchased predicate in this section
+
+
+%%%%% SECTION: shippingCost
+% Put your atomic statements for the shippingCost predicate in this section
+
+
+%%%% SECTION: taxRate
+% Put your atomic statements for the taxRate predicate in this section
+
+
+%%%%% SECTION: freeRegularShippingMin
+% Put your atomic statements for the freeRegularShippingMin predicate in this section
+
+
+%%%%% SECTION: freeExpressShippingMin
+% Put your atomic statements for the freeExpressShippingMin predicate in this section
+
+
+%%%%% SECTION: subtotal
+% Put your rules for the subtotal predicate in this section
+
+
+%%%%% SECTION: calculateBaseShipping
+% Put your rules for the calculateBaseShipping predicate in this section
+
+
+%%%%% SECTION: calculateShipping
+% Put your rules for the calculateShipping predicate in this section
+
+
+%%%%% SECTION: totalCost
+% Put your rules for the totalCost predicate in this section
+
+
---
+++ Assign1/q3_collab.pl
@@ -0,0 +1,30 @@
+% Enter the names of your group members below.
+% If you only have 2 group members, leave the last space blank
+%
+%%%%%
+%%%%% NAME: 
+%%%%% NAME:
+%%%%% NAME:
+%
+% Add the required rules in the corresponding sections. 
+% If you put the rules in the wrong sections, you will lose marks.
+%
+% You may add additional comments as you choose but DO NOT MODIFY the comment lines below
+%
+
+%%%%% SECTION: articleAuthor
+% Put your atomic statements for the articleAuthor predicate in this section
+
+
+%%%%% SECTION: articleTopic
+% Put your atomic statements for the articleTopic predicate in this section
+
+
+%%%%% SECTION: collabDist
+% Put your rules for collabDist in this section
+
+
+%%%% SECTION: collabDistWithAI
+% Put your rules for collabDistWithAI in this section
+
+
