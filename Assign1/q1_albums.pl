9aff4e42a5dcc21e6f80036d66cd02cdd5631c20
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2023-09-15 12:19:37 -0400

    Update and rename q1.pl to q1_albums.pl

---
+++ Assign1/q1_albums.pl
@@ -0,0 +1,35 @@
+% Enter the names of your group members below.
+% If you only have 2 group members, leave the last space blank
+%
+%%%%%
+%%%%% NAME: 
+%%%%% NAME:
+%%%%% NAME:
+%
+% Add the required rules in the corresponding sections. 
+% If you put the rules in the wrong sections, you may lose marks.
+%
+% You may add additional comments as you choose but DO NOT MODIFY the comment lines below
+%
+
+%%%%% SECTION: year
+% DO NOT EDIT THIS SECTION
+year(2000). year(2001). year(2002). year(2003). year(2004). year(2005). year(2006). year(2007). year(2008). year(2009).
+year(2010). year(2011). year(2012). year(2013). year(2014). year(2015). year(2016). year(2017). year(2018). year(2019).
+year(2020). year(2021). year(2022). year(2023).
+
+
+%%%%% SECTION: albumArtist
+
+
+
+
+
+%%%%% SECTION: albumLabel
+
+
+
+
+
+%%%%% SECTION: albumYear
+
