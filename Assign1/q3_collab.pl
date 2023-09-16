24d4194bb93fda857b4505c6ce3c54aa935f77b2
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2023-09-15 21:09:34 -0400

    Update q3_collab.pl
    collabDist work

--- Assign1/q3_collab.pl
+++ Assign1/q3_collab.pl
@@ -14,15 +14,15 @@
 
 % Authors
 author(paul_erdos).
-author(alice_smith).
-author(bob_jones).
-author(carol_davis).
-author(david_white).
-author(emily_brown).
-author(frank_green).
-author(grace_hall).
-author(henry_lee).
-author(irene_martin).
+author(stephen_king).
+author(william_shakespear).
+author(leo_tolstoy).
+author(yuri_slezkine).
+author(john_paul).
+author(joan_paul).
+author(james_paul).
+author(lebron_james).
+author(ben_simmons).
 
 % Publications
 publication(on_sets_of_distances_of_n_points).
@@ -35,16 +35,33 @@ publication(advanced_python_programming).
 % Put your atomic statements for the articleAuthor predicate in this section
 
 articleAuthor(Article, Author)
+articleAuthor(on_sets_of_distances_of_n_points, paul_erdos).
+articleAuthor(machine_learning_for_beginners, lebron_james).
+articleAuthor(cybersecurity_best_practices, stephen_king).
+articleAuthor(data_structures_in_c, leo_tolstoy).
+articleAuthor(advanced_python_programming, william_shakespear).
+articleAuthor(advanced_python_programming, lebron_james).
+
 
 %%%%% SECTION: articleTopic
 % Put your atomic statements for the articleTopic predicate in this section
 
 articleTopic(Article, Topic)
+articleTopic(on_sets_of_distances_of_n_points, programming)
+articleTopic(machine_learning_for_beginners, artificial_intelligence)
+articleTopic(cybersecurity_best_practices, cybersecurity)
+articleTopic(data_structures_in_c, programming)
+articleTopic(advanced_python_programming, programming)
 
 %%%%% SECTION: collabDist
 % Put your rules for collabDist in this section
 
-collabDist(Author1, Author2, MaxDist)
+collabDist(Author1, Author2, MaxDist) :-
+    MaxDist > 0,
+    articleAuthor(Article, Author1),  % Author1 is an author of Article
+    articleAuthor(Article, Author2),  % Author2 is also an author of Article
+    MaxDist1 is MaxDist - 1,          % Reduce MaxDist by 1
+    collabDist(Author1, Author2, MaxDist1).  % Recursively check with reduced MaxDist
 
 %%%% SECTION: collabDistWithAI
 % Put your rules for collabDistWithAI in this section
