23003bed3bc51c8b9d62fb45eda4c8ff769c15b3
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2023-09-15 21:09:55 -0400

    Update q3_collab.pl
    forgot to add periods

--- Assign1/q3_collab.pl
+++ Assign1/q3_collab.pl
@@ -34,7 +34,7 @@ publication(advanced_python_programming).
 %%%%% SECTION: articleAuthor
 % Put your atomic statements for the articleAuthor predicate in this section
 
-articleAuthor(Article, Author)
+articleAuthor(Article, Author).
 articleAuthor(on_sets_of_distances_of_n_points, paul_erdos).
 articleAuthor(machine_learning_for_beginners, lebron_james).
 articleAuthor(cybersecurity_best_practices, stephen_king).
@@ -46,12 +46,12 @@ articleAuthor(advanced_python_programming, lebron_james).
 %%%%% SECTION: articleTopic
 % Put your atomic statements for the articleTopic predicate in this section
 
-articleTopic(Article, Topic)
-articleTopic(on_sets_of_distances_of_n_points, programming)
-articleTopic(machine_learning_for_beginners, artificial_intelligence)
-articleTopic(cybersecurity_best_practices, cybersecurity)
-articleTopic(data_structures_in_c, programming)
-articleTopic(advanced_python_programming, programming)
+articleTopic(Article, Topic).
+articleTopic(on_sets_of_distances_of_n_points, programming).
+articleTopic(machine_learning_for_beginners, artificial_intelligence).
+articleTopic(cybersecurity_best_practices, cybersecurity).
+articleTopic(data_structures_in_c, programming).
+articleTopic(advanced_python_programming, programming).
 
 %%%%% SECTION: collabDist
 % Put your rules for collabDist in this section
