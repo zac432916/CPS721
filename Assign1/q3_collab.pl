6aed8aed1cb4a29844bfa0fb04feb00239e44f4d
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2023-09-16 15:52:48 -0400

    Update q3_collab.pl
    findall and list comprehension was a dead end of recursion

--- Assign1/q3_collab.pl
+++ Assign1/q3_collab.pl
@@ -44,11 +44,12 @@ publication(advanced_python_programming).
 %%%%% SECTION: articleAuthor
 % Put your atomic statements for the articleAuthor predicate in this section
 
-articleAuthor(Article, Author).
 articleAuthor(on_sets_of_distances_of_n_points, paul_erdos).
 articleAuthor(machine_learning_for_beginners, lebron_james).
 articleAuthor(cybersecurity_best_practices, stephen_king).
 articleAuthor(data_structures_in_c, leo_tolstoy).
+articleAuthor(data_structures_in_c, stephen_king).
+articleAuthor(data_structures_in_c, william_shakespear).
 articleAuthor(advanced_python_programming, william_shakespear).
 articleAuthor(advanced_python_programming, lebron_james).
 
@@ -56,7 +57,6 @@ articleAuthor(advanced_python_programming, lebron_james).
 %%%%% SECTION: articleTopic
 % Put your atomic statements for the articleTopic predicate in this section
 
-articleTopic(Article, Topic).
 articleTopic(on_sets_of_distances_of_n_points, programming).
 articleTopic(machine_learning_for_beginners, artificial_intelligence).
 articleTopic(cybersecurity_best_practices, cybersecurity).
@@ -66,28 +66,30 @@ articleTopic(advanced_python_programming, programming).
 %%%%% SECTION: collabDist
 % Put your rules for collabDist in this section
 
-% Base case: MaxDist is 0, and Author1 is the same as Author2.
-collabDist(Author, Author, 0).
+% Predicate to find all authors of an article who are not equal to Author1
 
-% Recursive rule: MaxDist is greater than 0, and there's a collaboration on an article.
+% Base case: Author is the same as Other Author
+collabDist(Author, Author, MaxDist) :-
+	Author = Author,
+	MaxDist >= 0.
+
+% Check if Author1 and Author2 worked on the same article
 collabDist(Author1, Author2, MaxDist) :-
     MaxDist > 0,
     articleAuthor(Article, Author1),  % Author1 is an author of Article
-    articleAuthor(Article, Author2),  % Author2 is also an author of Article
-    MaxDist1 is MaxDist - 1,          % Reduce MaxDist by 1
-    collabDist(Author1, Author2, MaxDist1).  % Recursively check with reduced MaxDist
+    articleAuthor(Article, Author2).  % Author2 is also an author of Article
 
-% Recursive rule: MaxDist is greater than 0, and there's a collaboration through a third author.
+% Check each author of article, until the author is author2
 collabDist(Author1, Author2, MaxDist) :-
     MaxDist > 0,
-    articleAuthor(Article, Author1),  % Author1 is an author of Article
-    articleAuthor(Article, Author3),  % Author3 is also an author of Article
-    Author1 \= Author3,               % Ensure Author1 and Author3 are different
-    collabDist(Author3, Author2, MaxDist1),  % Check if there's a path from Author3 to Author2
-    MaxDist1 is MaxDist - 1.                % Reduce MaxDist by 1
+    articleAuthor(Article, Author1),
+    articleAuthor(Article, OtherAuthor),  % Check if any other author wrote the article
+    Author1 \= Author2,  % Make sure the other author isn't the same author (no other collaborator) 
+    MaxDist1 is MaxDist - 1,  % Reduce MaxDist by 1
+    collabDist(OtherAuthor, Author2, MaxDist1).  % Recursively check with reduced MaxDist
+	
 
 %%%% SECTION: collabDistWithAI
 % Put your rules for collabDistWithAI in this section
 
-collabDist(Author1, Author2, MaxDist, AI)
-
+collabDist(Author1, Author2, MaxDist, AI).
