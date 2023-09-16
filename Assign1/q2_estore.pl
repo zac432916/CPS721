a1cd7159ff2fe12205aa2620a9559e46020051d2
hkartt <humzakhan7678@gmail.com>
2023-09-16 02:34:44 -0400

    q2 progress

--- Assign1/q2_estore.pl
+++ Assign1/q2_estore.pl
@@ -14,31 +14,37 @@
 
 %%%%% SECTION: cost
 % Put your atomic statements for the cost in this section
-
+cost(laptop, 600).
+cost(keyboard, 25).
+cost(monitor, 180).
 
 %%%%% SECTION: numPurchased
 % Put your atomic statements for the numPurchased predicate in this section
-
+numPurchased(laptop, 72).
+numPurchased(keyboard, 201).
+numPurchased(monitor, 33).
 
 %%%%% SECTION: shippingCost
 % Put your atomic statements for the shippingCost predicate in this section
-
+shippingCost(laptop, 7).
+shippingCost(keyboard, 10).
+shippingCost(monitor, 10).
 
 %%%% SECTION: taxRate
 % Put your atomic statements for the taxRate predicate in this section
-
+taxRate(0.13).
 
 %%%%% SECTION: freeRegularShippingMin
 % Put your atomic statements for the freeRegularShippingMin predicate in this section
-
+freeRegularShippingMin(35).
 
 %%%%% SECTION: freeExpressShippingMin
 % Put your atomic statements for the freeExpressShippingMin predicate in this section
-
+freeExpressShippingMin(200).
 
 %%%%% SECTION: subtotal
 % Put your rules for the subtotal predicate in this section
-
+subtotal(Sub) :-  Sub is ((72 * 600) + (201 * 25) + (33 * 180)).
 
 %%%%% SECTION: calculateBaseShipping
 % Put your rules for the calculateBaseShipping predicate in this section
