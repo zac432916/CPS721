88e7eb62a5b2242022f50d02c5586801ec36a347
hkartt <humzakhan7678@gmail.com>
2023-09-16 20:56:46 -0400

    q2 finished

--- Assign1/q2_estore.pl
+++ Assign1/q2_estore.pl
@@ -20,13 +20,13 @@ cost(monitor, 180).
 
 %%%%% SECTION: numPurchased
 % Put your atomic statements for the numPurchased predicate in this section
-numPurchased(laptop, 72).
-numPurchased(keyboard, 80).
-numPurchased(monitor, 33).
+numPurchased(laptop, 5).
+numPurchased(keyboard, 2).
+numPurchased(monitor, 5).
 
 %%%%% SECTION: shippingCost
 % Put your atomic statements for the shippingCost predicate in this section
-shippingCost(laptop, 7).
+shippingCost(laptop, 20).
 shippingCost(keyboard, 10).
 shippingCost(monitor, 10).
 
@@ -36,11 +36,11 @@ taxRate(0.13).
 
 %%%%% SECTION: freeRegularShippingMin
 % Put your atomic statements for the freeRegularShippingMin predicate in this section
-freeRegularShippingMin(35).
+freeRegularShippingMin(3000).
 
 %%%%% SECTION: freeExpressShippingMin
 % Put your atomic statements for the freeExpressShippingMin predicate in this section
-freeExpressShippingMin(200).
+freeExpressShippingMin(5000).
 
 %%%%% SECTION: subtotal
 % Put your rules for the subtotal predicate in this section
@@ -50,13 +50,19 @@ Sub is ((LaptopCost * LaptopAmount) + (KeyboardCost * KeyboardAmount) + (Monitor
 
 %%%%% SECTION: calculateBaseShipping
 % Put your rules for the calculateBaseShipping predicate in this section
-
+calculateBaseShipping(ShippingCost) :- shippingCost(laptop, LaptopCost), numPurchased(laptop, LaptopAmount), shippingCost(keyboard, KeyboardCost),
+numPurchased(keyboard, KeyboardAmount), shippingCost(monitor, MonitorCost), numPurchased(monitor, MonitorAmount), 
+ShippingCost is ((LaptopCost * LaptopAmount) + (KeyboardCost * KeyboardAmount) + (MonitorCost * MonitorAmount)).
 
 %%%%% SECTION: calculateShipping
 % Put your rules for the calculateShipping predicate in this section
-
+calculateShipping(regular, ShippingCost) :- calculateBaseShipping(X), ShippingCost is X.
+calculateShipping(express, ShippingCost) :- calculateBaseShipping(X), ShippingCost is (X * 1.5).
 
 %%%%% SECTION: totalCost
 % Put your rules for the totalCost predicate in this section
-
-
+totalCost(regular, Cost) :- subtotal(Sub), freeRegularShippingMin(Minimum), Sub >= Minimum -> totalCost(regular, Cost, free); totalCost(regular, Cost, notFree).
+totalCost(express, Cost) :- subtotal(Sub), freeExpressShippingMin(Minimum), Sub >= Minimum -> totalCost(express, Cost, free); totalCost(express, Cost, notFree).
+totalCost(_, Cost, free) :- taxRate(Tax), subtotal(Sub), Cost is Sub * (Tax + 1).
+totalCost(regular, Cost, notFree) :- taxRate(Tax), subtotal(Sub), calculateShipping(regular, ShippingCost), Cost is (ShippingCost + Sub) * (Tax + 1).
+totalCost(express, Cost, notFree) :- taxRate(Tax), subtotal(Sub), calculateShipping(express, ShippingCost), Cost is (ShippingCost + Sub) * (Tax + 1).
